a778880887aa47dd703dd67fc95535114d50d9b1
Everton gs <everton.gs.br1@gmail.com>
2023-07-29 19:28:42 -0300

    função validar url

--- funcao.php
+++ funcao.php
@@ -57,3 +57,9 @@ function validarEmail(string $email):bool
 {
   return filter_var($email,FILTER_VALIDATE_EMAIL);
 }
+
+
+function validarUrl(string $url):bool
+{
+   return  filter_var($url,FILTER_VALIDATE_URL);
+}
--- index.php
+++ index.php
@@ -7,3 +7,5 @@ echo '1';
 dataEnvioMensagem('2023-07-28 23:20:15');
 
 echo var_dump(validarEmail('evere@hotmail.com'));
+echo '<br>';
+echo var_dump(validarUrl('https://www.google.com.br/'));
